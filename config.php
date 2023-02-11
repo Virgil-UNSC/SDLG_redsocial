377f3c1987aa26bc2cd83ea1985018f18e9e61a8
Daniel Velazquez <61179330+Virgil-UNSC@users.noreply.github.com>
2023-02-11 07:52:30 -0600

    Uploaded script

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "admin_datahive";
+// MySQL Database Password
+$sql_db_pass = "yIbdK1Ua5Z";
+// MySQL Database Name
+$sql_db_name = "admin_tdlg";
+
+// Site URL
+$site_url = "https://tdlg.ml"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = ";D"; // Your purchase code, don't give it to anyone. 
+?>
